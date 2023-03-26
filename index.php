7736694fa94d21027a1bfe7e23ce9d0dc4e7b28e
Aleksei-Platonov <platonov.fishman1986@gmail.com>
2023-03-26 14:17:31 +0200

    correction files

--- index.php
+++ index.php
@@ -7,8 +7,20 @@ namespace App;
 include_once('./src/View.php');
 include_once('./src/utils/debug.php');
 
-$action = $_GET['action'] ?? null;
+const DEFAULT_ACTION = 'list';
+
+$action = $_GET['action'] ?? DEFAULT_ACTION;
+
+$viewParams = [];
+
+if ($action === 'create') {
+    $viewParams['resultCreate'] = 'Tworzymy nowa notatre';
+} else {
+    $viewParams['resultList'] = 'Wyswietlamy liste notatek';
+}
+
+
 
 
 $view = new View();
-$view->render($action);
+$view->render($action, $viewParams);
