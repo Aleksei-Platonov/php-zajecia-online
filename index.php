bc99f99ceeeff55a87ab2add9e511c78cdd12515
Aleksei-Platonov <platonov.fishman1986@gmail.com>
2023-03-27 20:35:01 +0200

    Add file controller.php and create controller of  index.php

--- index.php
+++ index.php
@@ -4,33 +4,11 @@ declare(strict_types=1);
 
 namespace App;
 
-include_once('./src/View.php');
+include_once('./src/Controller.php');
 include_once('./src/utils/debug.php');
 
-const DEFAULT_ACTION = 'list';
-
-$action = $_GET['action'] ?? DEFAULT_ACTION;
-
-$viewParams = [];
-
-if ($action === 'create') {
-    $page = 'create';
-    $created = false;
-    if (!empty($_POST)) {
-        $viewParams = [
-            'title' => $_POST['title'],
-            'description' => $_POST['description'],
-        ];
-        $created = true;
-    }
-    $viewParams['created'] = $created;
-} else {
-    $page = 'list';
-    $viewParams['resultList'] = 'Wyswietlamy liste notatek';
-}
 
 
 
-
-$view = new View();
-$view->render($page, $viewParams);
+$controller = new Controller($_GET, $_POST);
+$controller->run();
