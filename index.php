d0aef7bc5ce56bbd6eff0f4e19b4701b27bcec2a
Aleksei-Platonov <platonov.fishman1986@gmail.com>
2023-04-19 21:03:41 +0200

    fixed file index.php Database.php and added file AppException.php

--- index.php
+++ index.php
@@ -4,12 +4,18 @@ declare(strict_types=1);
 
 namespace App;
 
+require_once('./Exception/AppException.php');
 include_once('./src/Controller.php');
 include_once('./src/utils/debug.php');
 require_once('./config/config.php');
 
 
 
-Controller::initConfiguration($configuration);
-$controller = new Controller($_GET, $_POST);
-$controller->run();
+try {
+    Controller::initConfiguration($configuration);
+    $controller = new Controller($_GET, $_POST);
+    $controller->run();
+} catch (AppException $e) {
+    echo "<h1>Wystąpił błąd w aplikacji</h1>";
+    echo '<h3>' . $e->getMessage() . '</h3>';
+}
