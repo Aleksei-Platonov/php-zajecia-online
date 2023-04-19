026c09d71b273127b4b890aca9b5d21a6fec5031
Aleksei-Platonov <platonov.fishman1986@gmail.com>
2023-04-19 19:48:29 +0200

    fixed file index.html and added file Database.php in src folder

--- index.php
+++ index.php
@@ -6,9 +6,10 @@ namespace App;
 
 include_once('./src/Controller.php');
 include_once('./src/utils/debug.php');
+require_once('./config/config.php');
 
 
 
-
+Controller::initConfiguration($configuration);
 $controller = new Controller($_GET, $_POST);
 $controller->run();
