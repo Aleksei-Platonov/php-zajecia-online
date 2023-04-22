4192052ade64a4a4d6a3b4325fa128098c134f99
Aleksei-Platonov <platonov.fishman1986@gmail.com>
2023-04-22 13:56:37 +0200

    Finished Exception

--- index.php
+++ index.php
@@ -5,11 +5,16 @@ declare(strict_types=1);
 namespace App;
 
 require_once('./Exception/AppException.php');
+require_once('./Exception/ConfigurationException.php');
+require_once('./Exception/StorageException.php');
 include_once('./src/Controller.php');
 include_once('./src/utils/debug.php');
 require_once('./config/config.php');
 
-
+use App\Exception\AppException;
+use App\Exception\ConfigurationException;
+use App\Exception\StorageException;
+use Throwable;
 
 try {
     Controller::initConfiguration($configuration);
@@ -18,4 +23,6 @@ try {
 } catch (AppException $e) {
     echo "<h1>Wystąpił błąd w aplikacji</h1>";
     echo '<h3>' . $e->getMessage() . '</h3>';
+} catch (Throwable $e) {
+    echo "<h1>Wystąpił błąd w aplikacji</h1>"
 }
