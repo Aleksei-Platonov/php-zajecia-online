a41b11fd30b1117b4be91642b788c4853146689b
Aleksei-Platonov <platonov.fishman1986@gmail.com>
2023-04-23 15:02:24 +0200

    added file style.css and fixes other files

--- templates/pages/list.php
+++ templates/pages/list.php
@@ -1,16 +1,42 @@
 <div>
-    <div class="message">
-        <?php
-        if (!empty($params['before'])) {
-            switch ($params['before']) {
-                case 'created':
-                    echo "Notatka zostala utworzona";
-                    break;
+    <section>
+        <div class="message">
+            <?php
+            if (!empty($params['before'])) {
+
+                switch ($params['before']) {
+                    case 'created':
+                        echo "Notatka zostala utworzona";
+                        break;
+                }
             }
-        }
-        ?>
-    </div>
-    <b>
-        <?php echo $params['resultList'] ?? ""; ?>
-    </b>
+            ?>
+        </div>
+        <div class="tbl-header">
+            <table cellpadding="0" cellspacing="0" border="0">
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Tytul</th>
+                        <th>Data</th>
+                        <th>Opcje</th>
+                    </tr>
+                </thead>
+            </table>
+        </div>
+        <div class="tbl-content">
+            <table  cellpadding="0" cellspacing="0" border="0">
+                <tbody>
+                    <?php foreach ($params['notes'] as $note) : ?>
+                        <tr>
+                            <td><?php echo $note['id'] ?></td>
+                            <td><?php echo $note['title'] ?></td>
+                            <td><?php echo $note['created'] ?></td>
+                            <td>Option</td>
+                        </tr>
+                        <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
+    </section>
 </div>
