fa4f6e1a2144f634dcde5f477b202e2884f9fb54
Aleksei-Platonov <platonov.fishman1986@gmail.com>
2023-05-01 12:27:49 +0200

    added new file Request.php and fix warnings

--- src/Controller.php
+++ src/Controller.php
@@ -13,13 +13,13 @@ use App\Exception\NotFoundException;
 class Controller
 {
     const DEFAULT_ACTION = 'list';
-    private array $request;
+    private Request $request;
     private static $configuration = [];
     private Database $database;
     private View $view;
 
 
-    public function __construct(array $request)
+    public function __construct(Request $request)
     {
         $this->request = $request;
         $this->view = new View();
@@ -39,12 +39,10 @@ class Controller
         switch ($this->action()) {
             case 'create':
                 $page = 'create';
-                $data = $this->getRequestPost();
-                if (!empty($data)) {
-                    $created = true;
+                if ($this->request->hasPost()) {
                     $noteData = [
-                        'title' => $data['title'],
-                        'description' => $data['description'],
+                        'title' => $this->request->postParam('title'),
+                        'description' => $this->request->postParam('description'),
                     ];
                     $this->database->createNote($noteData);
                     header('Location: /?before=created');
@@ -53,8 +51,7 @@ class Controller
                 break;
                 case 'show':
                     $page = 'show';
-                    $data = $this->getRequestGet();
-                    $noteId = (int) $data['id'] ?? null;
+                    $noteId = (int) $this->request->getParam('id') ?? null;
                     if (!$noteId) {
                         header('Location: /?error=missingNoteId');
                         exit;
@@ -70,11 +67,10 @@ class Controller
                     break;
             default:
                 $page = 'list';
-                $data = $this->getRequestGet();
                 $viewParams = [
                     'notes' => $this->database->getNotes(),
-                    'before' => $data['before'] ?? null,
-                    'error' => $data['error'] ?? null,
+                    'before' => $this->request->getParam('before') ?? null,
+                    'error' => $this->request->getParam('error') ?? null,
                 ];
         }
 
@@ -83,15 +79,6 @@ class Controller
 
     private function action(): string
     {
-        $data = $this->getRequestGet();
-        return $data['action'] ?? self::DEFAULT_ACTION;
-    }
-    private function getRequestPost(): array
-    {
-        return $this->request['post'] ?? [];
-    }
-    private function getRequestGet(): array
-    {
-        return $this->request['get'] ?? [];
+        return $this->request->getParam('action') ?? self::DEFAULT_ACTION;
     }
 }
